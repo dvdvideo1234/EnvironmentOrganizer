a1fc5d8e59973c18179163b4bb7df5104663311d
Deyan Dobromirov <dvd_video@abv.bg>
2016-08-30 17:40:11 +0300

    Initial Commit

---
+++ lua/autorun/environment_organizer.lua
@@ -0,0 +1,6 @@
+-- https://wiki.garrysmod.com/page/Category:physenv
+local physenv = physenv
+
+local envAirDensityOld, envAirDensityNew = physenv.GetAirDensity()
+local envVecGravityOld, envVecGravityNew = physenv.GetGravity()
+local envPrSettingsOld, envPrSettingsNew = physenv.GetPerformanceSettings()
